01ff5f6a0535d02c3fcbb8d4d2fbf95d0d97a7d1
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2024-08-26 14:53:53 -0300

    changing cross origin

--- src/main/java/com/example/demo/FacebookController.java
+++ src/main/java/com/example/demo/FacebookController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://gabrielfurtadon.github.io")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class FacebookController {
